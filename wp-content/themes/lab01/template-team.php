2a1f18671c02a8bbea7dc579283b47fe0aea3e7b
Files90 <n.fabiani90@gmail.com>
2016-12-22 16:59:35 +0100

    Modiche HTML/PHP
    Modifiche HTML/PHP pagina “Team”:
    
    - Commentati tag <img> in attesa di essere caricate le immagini
    - Aggiunti due <div> per sostituire momentaneamente i due tag <img>

--- wp-content/themes/lab01/template-team.php
+++ wp-content/themes/lab01/template-team.php
@@ -3,11 +3,11 @@
 
 <div class="webdiary">
     <div class="webdiary2">
-        <img id="img1" src="<?php bloginfo('template_url'); ?> . /uploads/to/image.png" />
-        <div class="testo">
-            <h2><?php the_title() ?></h2>
-        </div>
-        <img id="img2" src="<?php bloginfo('template_url'); ?> . /path/to/image.png" />
+        <!-- <img id="img1" src="<?php bloginfo('template_url'); ?> . /uploads/to/image.png" /> -->
+        <div id="img1"></div><!-- Div test -->
+        <h2 class="title"><?php the_title() ?></h2>
+        <div id="img2"></div><!-- Div test -->
+        <!-- <img id="img2" src="<?php //bloginfo('template_url'); ?> . /path/to/image.png" /> -->
     </div>
 </div>
 
@@ -21,15 +21,19 @@
             <div class="teamsx">
                 <h3><?php echo $utenteSingolo->first_name; ?></h3>
                 <h4><?php echo $utenteSingolo->last_name; ?></h4>
-                <h5><?php $ruolo = get_field_object('ruolo', "user_".$utenteSingolo->ID);
-                   echo implode(", ",$ruolo['value']); ?></h5>
+                <h5><?php
+                    $ruolo = get_field_object('ruolo', "user_".$utenteSingolo->ID);
+                    echo implode(", ",$ruolo['value']); ?></h5>
                 <p><?php
-                    $tag = get_field_object('ruolo', "user_".$utenteSingolo->ID);
+                    $tag = get_field_object('tag', "user_".$utenteSingolo->ID);
                     echo $tag['value'];
-                 ?></p>
+                ?></p>
+                <a href="#">scopri ></a>
             </div>
-            <img src="" alt="" />
+            <img src="<?php the_field('immagine_profilo', 'user_'.$utenteSingolo->ID ); ?>" alt="Foto Profilo" title="Foto Profilo" />
         </div>
     <?php }
     ?>
 </div>
+
+<?php get_footer() ?>
