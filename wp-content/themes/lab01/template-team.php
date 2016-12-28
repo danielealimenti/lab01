c3c0c4359ff7ce697d08aaa219d2bfa11045a361
Files90 <n.fabiani90@gmail.com>
2016-12-28 13:00:43 +0100

    Aggiunte icone ondine e immagini users

--- wp-content/themes/lab01/template-team.php
+++ wp-content/themes/lab01/template-team.php
@@ -2,13 +2,7 @@
 <?php get_header(); ?>
 
 <div class="webdiary">
-    <div class="webdiary2">
-        <!-- <img id="img1" src="<?php bloginfo('template_url'); ?> . /uploads/to/image.png" /> -->
-        <div id="img1"></div><!-- Div test -->
-        <h2 class="title"><?php the_title() ?></h2>
-        <div id="img2"></div><!-- Div test -->
-        <!-- <img id="img2" src="<?php //bloginfo('template_url'); ?> . /path/to/image.png" /> -->
-    </div>
+    <h2><?php the_title() ?></h2>
 </div>
 
 <div id="team">
@@ -16,29 +10,27 @@
 
     $utenti = get_users();
 
-    foreach( $utenti as $utenteSingolo ) { ?>
+    foreach( $utenti as $singoloUtente ) { ?>
         <div class="team1">
             <div class="teamsx">
-                <h3><?php echo $utenteSingolo->first_name; ?></h3>
-                <h4><?php echo $utenteSingolo->last_name; ?></h4>
+                <h3><?php echo $singoloUtente->first_name; ?></h3>
+                <h4><?php echo $singoloUtente->last_name; ?></h4>
                 <h5><?php
-                    $ruolo = get_field_object('ruolo', "user_".$utenteSingolo->ID);
+                    $ruolo = get_field_object('ruolo', "user_".$singoloUtente->ID);
                     echo implode(", ",$ruolo['value']); ?></h5>
                 <p><?php
-                    $tag = get_field_object('tag', "user_".$utenteSingolo->ID);
+                    $tag = get_field_object('tags', "user_".$singoloUtente->ID);
                     echo $tag['value'];
                 ?></p>
-                <a href="#">scopri ></a>
+                <a href="<?php echo get_author_posts_url($singoloUtente->ID)?>">scopri ></a>
             </div>
             <?php
-            $profilo = get_field_object("immagine_profilo","user_$utenteSingolo->ID");
+                $profilo = get_field_object("immagine_profilo", "user_".$singoloUtente->ID);
             ?>
-
             <img src="<?php echo $profilo['value']['sizes']['profilo-team']; ?>" alt="Foto Profilo" title="Foto Profilo" />
-            
         </div>
     <?php }
-    ?>
+    ?><!-- END foreach -->
 </div>
 
 <?php get_footer() ?>
